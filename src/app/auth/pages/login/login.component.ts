89f1ef718489353dd3b3ad1820b2ae3e952bcf11
Nicolas La Torre <54626770+NikoLaTorre@users.noreply.github.com>
2022-06-21 01:16:55 -0300

    avance

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private _authService: AuthService, private router: Router) { 
     this.formulario = fb.group({
-      email: ["test@test.com", [Validators.required, Validators.email]],
+      email: ["a@a.com", [Validators.required, Validators.email]],
       password: ["123456", [Validators.required, Validators.minLength(6)]]
     })
   }
@@ -23,8 +23,10 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
+    this._authService.login(this.formulario.value).subscribe({next: res => console.log(res), complete: () => console.log(this._authService.user)});
+    console.log(this.formulario.value);
+    
     this.router.navigate(['/task']);
-    /* this._authService.login().subscribe( res => console.log(res)); */
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private _user: any;
+
+  get user(){
+    return this._user;
+  }
+
   constructor(private http: HttpClient) { }
 
-  login(): Observable<any>{
-    return this.http.post("http://localhost:3000/auth/login",{
-      email: "a@a.com",
-      password: "123456"
-    })
+  login(data: any): Observable<any>{
+    return this.http.post<any>("http://localhost:3000/auth/login", data)
+    .pipe(
+      tap( (res) => {
+
+        if (res.ok){
+          this._user = {
+            id: res.id,
+            username: res.username,
+            token: res.token
+          }
+        }
+        else {
+          this._user = null;
+        }
+      }),
+      map( res => res.ok)
+    )
   }
 }
